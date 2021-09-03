dc8b18cfa9a937a368d0ec5ffb527923c748a5fc
Vivaan Verma <doublevcodes@gmail.com>
2021-09-04 00:03:52 +0100

    Bump version 0.1.0 to 0.1.1

--- cloudflare_cli/__init__.py
+++ cloudflare_cli/__init__.py
@@ -5,5 +5,5 @@ COMMANDS_DIRECTORY: str = "commands"
 
 CLI: CloudflareCLI = CloudflareCLI(app_name=APP_NAME, commands_dir=COMMANDS_DIRECTORY)
 
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __all__: tuple[str, ...] = ("__version__", "CLI")
